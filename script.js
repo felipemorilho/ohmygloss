8a8fa019f88378378c5539f36b50e2fcc8dfb620
Felipe Morilho <felipemdecastro@gmail.com>
2024-05-14 13:34:30 -0300

    feat: add interactive close popup when click outside it

--- script.js
+++ script.js
@@ -38,39 +38,35 @@ document.addEventListener('DOMContentLoaded', function() {
         const emailInput = document.getElementById('email');
         const messageInput = document.getElementById('message');
 
-        // Define regex patterns for validation
         const nameRegex = /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         const messageRegex = /^[a-zA-Z0-9\s.,?!]{1,200}$/;
 
-        // Function to validate each field
         const validateField = (input, regex) => regex.test(input.value);
 
-        // Validate nome field
         if (!validateField(nameInput, nameRegex)) {
             alert('Por favor, insira um nome válido.');
             return;
         }
 
-        // Validate email field
         if (!validateField(emailInput, emailRegex)) {
             alert('Por favor, insira um endereço de email válido.');
             return;
         }
 
-        // Validate mensagem field
         if (!validateField(messageInput, messageRegex)) {
             alert('Por favor, insira uma mensagem válida.');
             return;
         }
 
-        // If all fields are valid, submit the form
         alert('Formulário enviado com sucesso!');
         form.submit();
     });
 });
 
 const btnPopup = document.getElementById('btn__popup')
+const popupElement = document.getElementById('initial__popup')
+const popup = document.querySelector('.popup')
 
 btnPopup.addEventListener('click', () => {
     fecharPopup();
@@ -78,12 +74,20 @@ btnPopup.addEventListener('click', () => {
 
 document.addEventListener("DOMContentLoaded", function() {
     exibirPopup();
+
+    document.addEventListener('click', function(event) {
+        if (!popup.contains(event.target) && event.target === popupElement) {
+            fecharPopup();
+        }
+    });
+    
 });
 
+
 function exibirPopup() {
-    document.getElementById('initial__popup').style.display = 'flex';
+    popupElement.style.display = 'flex';
 }
 
 function fecharPopup() {
-    document.getElementById('initial__popup').style.display = 'none';
+    popupElement.style.display = 'none';
 }
